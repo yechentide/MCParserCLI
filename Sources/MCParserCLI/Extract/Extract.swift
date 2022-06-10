bccee6a956d7cc2778bedc6c064e13937a0cbf00
yechentide <yechentide@gmail.com>
2022-06-10 19:11:07 +0900

    refac: delete unnecessary comments

--- Sources/MCParserCLI/Extract/Extract.swift
+++ Sources/MCParserCLI/Extract/Extract.swift
@@ -10,13 +10,10 @@ import LvDBWrapper
 
 extension MCParserCLI {
     struct Extract: ParsableCommand {
-        //    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
-        //    var count: Int?
-        
-        @Option(name: .customLong("db"), help: "The path of a db directory")
+        @Option(name: .customLong("db"), help: "The path of a db directory.")
         var dbDirPath: String
         
-        @Option(name: .customLong("output"), help: "The path where to output")
+        @Option(name: .customLong("output"), help: "The path where to output.")
         var outDirPath: String
         
         func run() {
--- Sources/MCParserCLI/Root.swift
+++ Sources/MCParserCLI/Root.swift
@@ -15,33 +15,4 @@ struct MCParserCLI: ParsableCommand {
             Decode.self
         ]
     )
-    
-//    @Flag(help: "Include a counter with each repetition.")
-//    var includeCounter = false
-
-//    @Flag(name: .shortAndLong)
-//    var version = false
-    
-//    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
-//    var count: Int?
-    
-//    @Argument(help: "The phrase to repeat.")
-//    var phrase: String
-    
-//    mutating func run() throws {
-//        if version {
-//            print(MCParserCLI.configuration.version)
-//        }
-//
-//        MCParserCLICore.greet()
-//        let repeatCount = count ?? 2
-//
-//        for i in 1...repeatCount {
-//            if includeCounter {
-//                print("\(i): \(phrase)")
-//            } else {
-//                print(phrase)
-//            }
-//        }
-//    }
 }
